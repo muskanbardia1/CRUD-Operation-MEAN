04e386b7d4d2eb97a16e5c21a16e53956a40bc42
unknown <muskanbardia98@gmail.com>
2021-05-03 18:42:36 +0530

    CRUD operation MEAN

---
+++ NodeJS/models/employee.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Employee = mongoose.model('Employee', {
+  name: { type: String },
+  position: { type: String },
+  office: { type: String },
+  salary: { type: Number }
+});
+
+module.exports = {Employee};
